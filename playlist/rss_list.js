869cd252075172b815f6e32e4f9d1af1f581b83a
Dstray <lixt12@qq.com>
2015-08-18 16:48:18 +0800

    update rss list

--- playlist/rss_list.js
+++ playlist/rss_list.js
@@ -7,6 +7,6 @@
 var listpath = "http://carboncook.github.io/WebPlayer/playlist/"
 var rsslist = {
     "Sample": "sample.rss",
-    "PRESTIGE": "prestige_100.rss",
+    "PRESTIGE": "prestige_080.rss",
     "RSS 02": "rss_02.rss"
 };
