7d7dd5a8922567ebb6b44227e0888604904d1e6c
Dstray <lixt12@qq.com>
2015-08-22 11:37:17 +0800

    update rss list

--- playlist/listgener2.py
+++ playlist/listgener2.py
@@ -1,3 +1,11 @@
 import urllib2 as urllib
 import codecs, re, time
-import 
+from pyquery import PyQuery as pq
+#from lxml import etree
+
+def listgener2():
+	doc = pq(url = 'http://carboncook.github.io/WebPlayer/', opener = lambda url, **kw: urllib.urlopen(url).read())
+	print doc("title").html()
+
+if __name__ == '__main__':
+	listgener2()
--- playlist/rss_list.js
+++ playlist/rss_list.js
@@ -8,7 +8,7 @@ var listpath = "http://carboncook.github.io/WebPlayer/playlist/"
 var rsslist = {
     "Sample": "sample.rss",
     "PRESTIGE 1": "prestige_0080.rss",
-    "PRESTIGE 2": "prestige_0160.rss",
+    "PRESTIGE 2": "prestige_0170.rss",
     "PRESTIGE 3": "prestige_0210.rss",
     "PRESTIGE 4": "prestige.rss"
 };
